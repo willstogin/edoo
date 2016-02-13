6a6d3843479f1933b78e46d727901e0a6fc8d4b0
John Stogin <jstogin@gmail.com>
2016-02-13 16:05:51 -0500

    Added length, width, height to block.

--- objects/block.js
+++ objects/block.js
@@ -6,17 +6,21 @@ var Block = function(xml_node) {
 // Constructor //
 /////////////////
 
+    var length = 1;
+    var width = 1;
+    var height = 1;
+
+    if (n.hasAttribute('length'))
+	size = n.getAttribute('length');
+    if (n.hasAttribute('width'))
+	size = n.getAttribute('width');
+    if (n.hasAttribute('height'))
+	size = n.getAttribute('height');
     
-    // TODO insert construction code here
-    console.log("creating a block!");
-
-    console.log(n.getAttribute('color'));
-    console.log(n.children);
-
-    var size = 1;
-    if (n.hasAttribute('size'))
-	size = n.getAttribute('size');
-    var self = BABYLON.Mesh.CreateBox("",size,scene);
+    var self = BABYLON.Mesh.CreateBox("",1,scene);
+    self.scaling.x = length;
+    self.scaling.y = width;
+    self.scaling.z = height;
     
 
 ///////////////////////
