ed89c6db69625a87bfef7bef274d7b532063e533
John Stogin <jstogin@gmail.com>
2016-02-13 04:53:59 -0500

    Added xml parser.

---
+++ xml_parser/parser.js
@@ -0,0 +1,57 @@
+
+// Basic DOM routines
+
+function childNodes(node){
+    var children = [];
+    subNode = node.firstChild;
+
+    while (subNode != null) {
+	if (subNode.nodeType == 1)
+	    children.push(subNode);
+	subNode = subNode.nextSibling;
+    }
+    return children;
+}
+
+// The parser
+
+/*
+function handleNode(node) {
+    console.log(node.tagName);
+    var children = childNodes(node);
+    for (var i=0; i<children.length; i++) {
+	handleNode(children[i]);
+    }
+}
+*/
+
+function parseXml(txt) {
+    // Note: The variable txt is supposed to only have one root node. So I wrap it in zzz.
+    txt = "<zzz>"+txt+"</zzz>";
+    var xmlDoc;
+    if (window.DOMParser) {
+	var parser=new DOMParser();
+	xmlDoc=parser.parseFromString(txt,"text/xml");
+    } else {
+	// Internet Explorer
+	xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+	xmlDoc.async=false;
+	xmlDoc.loadXML(txt);
+    }
+
+    // All the nodes that are roots.
+    rootNodes = childNodes(childNodes(xmlDoc)[0])
+    for (var i=0; i<rootNodes.length; i++) {
+	//handleNode(rootNodes[i]);
+	// TODO: Do stuff with rootNodes[i] here. (ie. call a function)
+    }
+}
+
+function parseXmlTextArea() {
+
+    var xmlTextArea = document.getElementById("xml");
+    var xml = xmlTextArea.value;
+
+    parseXml(xml);
+
+}
