0b01097e308cc3d3bddb8bffd6a1927d427178f9
William <williamstogin2018@u.northwestern.edu>
2016-02-14 03:41:30 -0600

    fixed turret point for good

--- objects/turret.js
+++ objects/turret.js
@@ -35,17 +35,14 @@ var Turret = function(xml_node, parent) {
     
     var self = BABYLON.Mesh.CreateBox(id,0,scene);
     self.isVisible = false;
-    
-    // Create the hemisphere
-     var hemisphere = BABYLON.MeshBuilder.CreateSphere('', {diameter: 2*radius, slice: .5}, scene);   
+      
     
     // Create the cylinder
-	var barrel = BABYLON.Mesh.CreateCylinder("",length,radius/4, radius/4, 0, 0,scene, false, BABYLON.Mesh.DOUBLESIDE);
-        barrel.position = new BABYLON.Vector3(0, length/2, 0);
+	var barrel; //= BABYLON.Mesh.CreateCylinder("",length,radius/4, radius/4, 0, 0,scene, false, BABYLON.Mesh.DOUBLESIDE);
+//        barrel.position = new BABYLON.Vector3(0, length/2, 0);
     
     setLatLong(lat, long);
-    
-    hemisphere.parent = self;    
+      
     
     // Add physics
     hemisphere.setPhysicsState({ imposter: BABYLON.PhysicsEngine.BoxImposter, mass:1, restitution: 0});
@@ -61,26 +58,41 @@ var Turret = function(xml_node, parent) {
 // Private Functions //
 ///////////////////////
     function setLatLong(la, lo) { // Assume latitude is in radians (up from x-y plane), assume longitude in radians (counterclockwise from positive z)        
-        if (isFirstSet) {
-            lat = 0;
-            long = 0;
-        }
-        
-        // Find difference between previous and current angles
-        la = la - lat;
-        lo = lo - long;
-        
-        
+//        if (isFirstSet ) {
+//            lat = 0;
+//            long = 0;
+//        }
+        barrel = BABYLON.Mesh.CreateCylinder("",length,radius/4, radius/4, 0, 0,scene, false, BABYLON.Mesh.DOUBLESIDE);
+        barrel.position = new BABYLON.Vector3(0, length/2, 0);
+        // Find difference between previous and current angles       
         la = Math.PI/2 - la; // la is now in terms of down from vertical
+//        la = la - lat;
+//        lo = lo - long; 
+        
+        
         // Rotate forward
+        // Make barrel's parent be an empty pivot
+//        var EmptyPivot = new BABYLON.Mesh("pivos", scene);
+//        EmptyPivot.position.x = barrel.position.x - length/2 * Math.sin(lat);
+//        EmptyPivot.position.y = barrel.position.y - length/2*Math.cos(lat);
+//        EmptyPivot.position.z = barrel.position.z - length/2 * Math.sin(long);
+//        barrel.parent = EmptyPivot;
+//        EmptyPivot.rotate(BABYLON.Axis.X, la, BABYLON.Space.Local);
+//        EmptyPivot.rotate.x = la;
+        
         barrel.rotate(BABYLON.Axis.X, la, BABYLON.Space.Local);
         // Return to proper place
         barrel.position.z = barrel.position.z + length/2*(Math.sin(la));
         barrel.position.y = barrel.position.y - length/2*(1 - Math.cos(la));
+        // Create the hemisphere
+        hemisphere = BABYLON.MeshBuilder.CreateSphere('', {diameter: 2*radius, slice: .5}, scene); 
+        barrel.parent = hemisphere;
         
-        var projection = length * Math.cos(la);
+//        var projection = length * Math.cos(la);
         hemisphere.rotate(BABYLON.Axis.Y, lo, BABYLON.Space.Local);
         barrel.parent = hemisphere;
+
+        hemisphere.parent = self;  
         
         lat = lat + la;
         long = lo + long;
@@ -120,7 +132,8 @@ var Turret = function(xml_node, parent) {
     }
     
     self.setAngles = function (la, lo) {        
-        //barrel.dispose();
+        hemisphere.dispose();
+        barrel.dispose();
         
         setLatLong(la, lo);
     }
